47c72d59cd28738c193f41807069c4085b29d265
mvicbar <mariavba@ucm.es>
2019-02-15 20:20:15 +0100

    Clase del algoritmo creada, varios errores a corregir
    Errores:
    1. Sobreescritura de padres
    2. Orden de los nodos
    3. Cálculo de costes

--- Práctica 1 - Algoritmo A Star/Astar_algorithm/Astar_algorithm/Astar.cpp
+++ Práctica 1 - Algoritmo A Star/Astar_algorithm/Astar_algorithm/Astar.cpp
@@ -3,99 +3,142 @@
 #include <fstream>
 #include <vector>
 #include <utility>
+#include <math.h>
 
-template <class T>
-class Nodo {
+using namespace std;
+
+class Astar {
+private:
+
+	class Nodo {
+	public:
+		Nodo() : fila(-1), columna(-1), coste(-1), padre(NULL) {};
+		Nodo(int fila, int columna, double coste, Nodo *padre) :
+			fila(fila), columna(columna), coste(coste), padre(padre) {};
+
+		int fila;
+		int columna;
+		double coste;
+		Nodo* padre;
+
+		/*float coste() {
+			return coste;
+		}*/
+	};
+
+	int F, C;
+	queue<Nodo> cerrada;
+	priority_queue<Nodo> abierta;
+
+	Nodo nodo_ini, meta;
+	
+	const vector<pair<int, int>> dirs = { { 0,1 },{ 0,-1 },{ 1,0 },{ -1,0 },{ 1,1 },{ -1,-1 },{ 1,-1 },{ -1,1 } };
+
+	bool correct(int i, int j) {
+		return 0 <= i && i < F && 0 <= j && j < C;
+	}
+
 public:
-	Nodo(int fila, int columna, T coste, Nodo<T> *padre) :
-		fila(fila), columna(columna), coste(coste), padre(padre) {};
 
-	int fila;
-	int columna;
-	T coste;
-	Nodo* padre;
+	Astar(vector<string> const& mapa, pair<int, int> const& inicio, pair<int, int> const& meta) : F(mapa.size()), C(mapa[0].size()),
+		nodo_ini(inicio.first, inicio.second, sqrt(pow((inicio.first - meta.first), 2) + pow((inicio.second - meta.second), 2)), nullptr) {
+
+		abierta.push(nodo_ini);
+		bool meta_seleccionada = false;
+
+		while (!abierta.empty() && !meta_seleccionada) {
+
+			Nodo n = abierta.top(); abierta.pop();	//	FALLO: eligen al nodo con mayor coste, en vez de el de menor coste
+			cerrada.push(n);
+
+			meta_seleccionada = n.fila == meta.first && n.columna == meta.second;
+
+			for (auto d : dirs) {
+				int nf = n.fila + d.first;
+				int nc = n.columna + d.second;
+
+				if (correct(nf, nc) && mapa[nf][nc] != 'X' && (nf != n.fila || nc != n.columna)) {	//Comprobar que la nueva casilla no es la del padre
+
+					double coste_nodo = sqrt(pow((nf - meta.first), 2) + pow((nc - meta.second), 2));
+
+					Nodo padre = n;	//FALLO: los padres se sobreescriben
+					Nodo nuevo = { nf, nc, coste_nodo + n.coste + 1, &padre };	//	Arreglar el coste de cada nodo
+
+					abierta.push(nuevo);
+
+				}
+			}
+		}
+
+		if (meta_seleccionada) {
+			Nodo n = cerrada.back();
+			vector<pair<int, int>> camino;
+
+			while (n.padre != NULL) {
+				camino.push_back({ n.padre->fila, n.padre->columna });
+				n = *n.padre;
+			}
+
+			std::cout << "Camino óptimo: \n";
+
+			for (int i = camino.size() - 1; i >= 0; --i) {
+				std::cout << "(" << camino[i].first << ", " << camino[i].second << ") -> ";
+			}
+		}
+		else
+			std::cout << "No se ha encontrado el camino óptimo\n";
+	}
+
+	friend bool operator<(Astar::Nodo const n1, Astar::Nodo const n2);
 };
 
-bool operator<(Nodo<int> n1, Nodo<int> n2) {
+vector<string> construirTablero(int F, int C, vector<pair<int, int>>& prohibidas) {
+	vector<string> tablero(F);
+
+	//	Inicialización del tablero sin casillas prohibidas
+	for (int i = 0; i < F; ++i)
+		for (int j = 0; j < C; ++j)
+			tablero[i].push_back('.');
+
+	//	Colocación de las casillas prohibidas
+	for (auto p : prohibidas) 
+		tablero[p.first][p.second] = 'X';
+
+	return tablero;
+}
+
+bool operator<(Astar::Nodo const n1, Astar::Nodo const n2) {
 	return n1.coste < n2.coste;
 }
 
-std::queue<Nodo<int>> cerrada;
-std::priority_queue<Nodo<int>> abierta;
-
-const std::vector<std::pair<int, int>> dirs = { {0,1}, {0,-1}, {1,0}, {-1,0}, {1,1}, {-1,-1}, {1,-1}, {-1,1} };
-
-void Astar(Nodo<int>& meta, int M, int N) {
-	bool meta_seleccionada = false;
-	char mapa[4][4] = { '.' };
-
-	while (!abierta.empty() && !meta_seleccionada) {
-
-		Nodo<int> n = abierta.top(); abierta.pop();
-		cerrada.push(n);
-
-		meta_seleccionada = n.fila == meta.fila && n.columna == meta.columna;
-
-		for (std::pair<int, int> d : dirs) {
-			int nf = n.fila + d.first;
-			int nc = n.columna + d.second;
-
-			if (nf >= 0 && nf < M && nc >= 0 && nc < N && mapa[nf][nc] != 'X' && (nf != n.fila || nc != n.columna)) {
-
-				int coste_nodo = sqrt((nf - meta.fila) ^ 2 + (nc - meta.columna) ^ 2);
-
-				Nodo<int> nuevo = { nf, nc, coste_nodo + n.coste + 1, &n };
-
-				abierta.push(nuevo);
-
-			}
-		}
-	}
-
-	if (meta_seleccionada) {
-		Nodo<int> n = cerrada.back();
-		std::vector<std::pair<int, int>> camino;
-
-		while (n.padre != NULL) {
-			camino.push_back({ n.padre->fila, n.padre->columna});
-			n = *n.padre;
-		}
-
-		std::cout << "Camino óptimo: \n";
-
-		for (int i = camino.size()-1; i >= 0; --i) {
-			std::cout << "(" << camino[i].first << ", " << camino[i].second << ") -> ";
-		}
-	}
-	else
-		std::cout << "No se ha encontrado el camino óptimo\n";
-
-	
-}
-
-
 int main() {
-	int N, M;
-	int fila_ini, columna_ini;
-	int fila_meta, columna_meta;
+	int F, C;
+	pair<int, int> inicio, meta;
+	int f, c;
+	vector<pair<int, int>> prohibidas;
 
-	/*std::cout << "Introduce las dimensiones de la matriz (M x N): \n";
-	std::cin >> M >> N;*/
+	std::cout << "Introduce las dimensiones de la matriz (F x C): ";
+	std::cin >> F >> C;
 
-	std::cout << "Introduce la fila y la columna de inicio: \n";
-	std::cin >> fila_ini >> columna_ini;
+	cout << "Introduce la fila y la columna de inicio: ";
+	cin >> f >> c;
+	inicio = { f - 1, c - 1 };
 
-	std::cout << "Introduce la fila y la columna de meta: \n";
-	std::cin >> fila_meta >> columna_meta;
+	cout << "Introduce la fila y la columna de meta: ";
+	cin >> f >> c;
+	meta = { f - 1, c - 1 };
 
-	int coste_inicio = sqrt((fila_ini - fila_meta) ^ 2 + (columna_ini - columna_meta) ^ 2);
+	cout << "Introduce las casillas prohibidas (0 0 para parar): ";
+	do {
+		cin >> f >> c;
+		prohibidas.push_back({ f - 1, c - 1 });
+	} while (f != 0);
 
-	Nodo<int> inicio = { fila_ini, columna_ini, coste_inicio, nullptr };
-	Nodo<int> meta = { fila_meta, columna_meta, 0, nullptr };
+	prohibidas.pop_back();
+	vector<string> mapa = construirTablero(F, C, prohibidas);
 
-	abierta.push(inicio);
-
-	Astar(meta, 4, 4);
+	Astar(mapa, inicio, meta);
 
 	return 0;
 }
+
